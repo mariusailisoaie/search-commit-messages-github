c95f8bddac9a12ea52d217e17d51d9bd75baedf4
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-07-16 17:11:55 +0200

    Fix import for SearchCommits component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import SearchCommits from './components/SearchCommits';
+import SearchCommits from './components/SearchCommits/SearchCommits';
 
 function App() {
   return (
