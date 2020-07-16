e9a3e78e5c8562b499f7513796e3777fe334c787
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-07-16 17:11:36 +0200

    Move SearchCommits component into SearchCommits folder. Add functionality for displaying all commits of a repo

---
+++ src/components/SearchCommits/SearchCommits.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+import './SearchCommits.scss';
+
+import axios from 'axios';
+
+const SearchCommits = () => {
+  const [combinedCommitsArray, setCombinedCommitsArray] = useState([]);
+  const [repoDetails, setRepoDetails] = useState({ owner: 'mariusailisoaie', repo: 'crwn-clothing' });
+
+  const allCommits = [];
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    const commits = await axios.get(`https://api.github.com/repos/${ repoDetails.owner }/${ repoDetails.repo }/commits?page=1&per_page=100`);
+
+    if (commits.headers.link) {
+      const linkLength = parseInt(commits.headers.link.split(',')[1].split('?page=')[1].split('&')[0]);
+
+      for (let i = 1; i < linkLength + 1; i++) {
+        const commitsPerPage = await axios.get(`https://api.github.com/repos/${ repoDetails.owner }/${ repoDetails.repo }/commits?page=${ i }&per_page=100`);
+        allCommits.push(commitsPerPage.data);
+      }
+
+      setCombinedCommitsArray([...allCommits.flat()]);
+    } else {
+      setCombinedCommitsArray([...commits.data]);
+    }
+  }
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+
+    setRepoDetails({ ...repoDetails, [name]: value });
+  }
+
+  return (
+    <div className='container'>
+      <div className='form-container'>
+        <form onSubmit={ handleSubmit }>
+          <input onChange={ handleChange } type='text' name='owner' placeholder='Owner' />
+          <input onChange={ handleChange } type='text' name='repo' placeholder='Repo' />
+          <input type='submit' value='Submit' hidden />
+        </form>
+      </div>
+
+      <div className='commits-container'>
+        { combinedCommitsArray.length ? <div>This repo has a total of { combinedCommitsArray.length } commits.</div> : null }
+
+        { combinedCommitsArray.map((commit, index) => <div key={ index } className='commit'>{ commit.commit.message }</div>) }
+      </div>
+    </div>
+  )
+}
+
+export default SearchCommits;
